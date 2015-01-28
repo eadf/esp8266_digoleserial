57ca3dd2219d4e244234ad6704b10cb558b2d5fd
EAD Fritz <ead.fritz@gmail.com>
2015-01-28 12:24:41 +0100

    added easygpio

--- Makefile
+++ Makefile
@@ -24,7 +24,7 @@ ESPPORT		?= /dev/ttyUSB0
 TARGET		= app
 
 # which modules (subdirectories) of the project to include in compiling
-MODULES		= driver digoleserial bigint tachometer user
+MODULES		= driver easygpio digoleserial bigint tachometer user
 EXTRA_INCDIR    = include $(SDK_BASE)/../include
 
 # libraries used in this project, mainly provided by the SDK
--- tachometer/tachometer.c
+++ tachometer/tachometer.c
@@ -9,6 +9,7 @@
 #include "gpio.h"
 #include "mem.h"
 #include "tachometer/tachometer.h"
+#include "easygpio/easygpio.h"
 #include "os_type.h"
 
 #define tachometer_micros (0x7FFFFFFF & system_get_time())
@@ -26,7 +27,6 @@ static void tachometer_disableInterrupt(void);
 static void tachometer_enableInterrupt(void);
 static void tachometer_intr_handler(void);
 static void tachometer_timerFunc(void);
-static bool tachometer_setupInterrupt(uint8 gpio_pin, void (*interruptHandler)(void));
 
 static void
 tachometer_disableInterrupt(void) {
@@ -78,94 +78,6 @@ tachometer_timerFunc(void) {
   counter += 1;
 }
 
-/**
- * Sets the 'gpio_pin' pin as a GPIO and sets the interrupt to trigger on that pin
- */
-static bool ICACHE_FLASH_ATTR
-tachometer_setupInterrupt(uint8 gpio_pin, void (*interruptHandler)(void)) {
-  uint8 gpio_func;
-  uint32 gpio_name;
-
-  if (gpio_pin == 6 || gpio_pin == 7 || gpio_pin == 8 || gpio_pin == 11 || gpio_pin >= 17) {
-    os_printf("tachometer_setupInterrupt Error: There is no GPIO%d, check your code\n", gpio_pin);
-    return false;
-  }
-  if (gpio_pin == 16) {
-    os_printf("tachometer_setupInterrupt Error: GPIO16 does not have interrupts\n");
-    return false;
-  }
-
-  if (gpio_pin == 0) {
-    gpio_func = FUNC_GPIO0;
-    gpio_name = PERIPHS_IO_MUX_GPIO0_U;
-  }
-  if (gpio_pin == 1) {
-    gpio_func = FUNC_GPIO1;
-    gpio_name = PERIPHS_IO_MUX_U0TXD_U;
-  }
-  if (gpio_pin == 2) {
-    gpio_func = FUNC_GPIO2;
-    gpio_name = PERIPHS_IO_MUX_GPIO2_U;
-  }
-  if (gpio_pin == 3) {
-    gpio_func = FUNC_GPIO3;
-    gpio_name = PERIPHS_IO_MUX_U0RXD_U;
-  }
-  if (gpio_pin == 4) {
-    gpio_func = FUNC_GPIO4;
-    gpio_name = PERIPHS_IO_MUX_GPIO4_U;
-  }
-  if (gpio_pin == 5) {
-    gpio_func = FUNC_GPIO5;
-    gpio_name = PERIPHS_IO_MUX_GPIO5_U;
-  }
-  if (gpio_pin == 9) {
-    gpio_func = FUNC_GPIO9;
-    gpio_name = PERIPHS_IO_MUX_SD_DATA2_U;
-  }
-  if (gpio_pin == 10) {
-    gpio_func = FUNC_GPIO10;
-    gpio_name = PERIPHS_IO_MUX_SD_DATA3_U;
-  }
-  if (gpio_pin == 12) {
-    gpio_func = FUNC_GPIO12;
-    gpio_name = PERIPHS_IO_MUX_MTDI_U;
-  }
-  if (gpio_pin == 13) {
-    gpio_func = FUNC_GPIO13;
-    gpio_name = PERIPHS_IO_MUX_MTCK_U;
-  }
-  if (gpio_pin == 14) {
-    gpio_func = FUNC_GPIO14;
-    gpio_name = PERIPHS_IO_MUX_MTMS_U;
-  }
-  if (gpio_pin == 15) {
-    gpio_func = FUNC_GPIO15;
-    gpio_name = PERIPHS_IO_MUX_MTDO_U;
-  }
-
-  ETS_GPIO_INTR_ATTACH(interruptHandler, NULL);
-  ETS_GPIO_INTR_DISABLE();
-
-  PIN_FUNC_SELECT(gpio_name, gpio_func);
-  //disable pull downs
-  PIN_PULLDWN_DIS(gpio_name);
-  //disable pull ups
-  PIN_PULLUP_DIS(gpio_name);
-  // disable output
-  GPIO_DIS_OUTPUT(gpio_pin);
-
-  gpio_register_set(GPIO_PIN_ADDR(gpio_pin), GPIO_PIN_INT_TYPE_SET(GPIO_PIN_INTR_DISABLE)
-                    | GPIO_PIN_PAD_DRIVER_SET(GPIO_PAD_DRIVER_DISABLE)
-                    | GPIO_PIN_SOURCE_SET(GPIO_AS_PIN_SOURCE));
-
-  //clear gpio14 status
-  GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, BIT(gpio_pin));
-  ETS_GPIO_INTR_ENABLE();
-
-  return true;
-}
-
 void ICACHE_FLASH_ATTR
 tachometer_init(uint8_t ioPin) {
   tachometer_pin = ioPin;
@@ -174,7 +86,7 @@ tachometer_init(uint8_t ioPin) {
   os_timer_disarm(&tachometer_timer);
   os_timer_setfn(&tachometer_timer, (os_timer_func_t *) tachometer_timerFunc, NULL);
 
-  if (tachometer_setupInterrupt(tachometer_pin, tachometer_intr_handler)) {
+  if (easygpio_setupInterrupt(tachometer_pin, false, false, tachometer_intr_handler)) {
     // start the poll/sample timer
     os_timer_arm(&tachometer_timer, TACHOMETER_POLL_TIME, 1);
     tachometer_enableInterrupt();
